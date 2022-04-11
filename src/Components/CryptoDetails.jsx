2dd07407f0f36cf43b2cd68464259e71e4ab365b
Marko Livaic <makifrane@gmail.com>
2022-04-11 21:22:38 +0200

    push

--- src/Components/CryptoDetails.jsx
+++ src/Components/CryptoDetails.jsx
@@ -26,7 +26,7 @@ const CryptoDetails = () => {
   const { coinId } = useParams();
   const [timePeriod, setTimePeriod] = useState("7d");
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
-  const { data: coinHistory } = useGetCryptoDetailsQuery(coinId, timePeriod);
+  const { data: coinHistory } = useGetCryptoHistoryQuery(coinId, timePeriod);
   const cryptoDetails = data?.data?.coin;
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
